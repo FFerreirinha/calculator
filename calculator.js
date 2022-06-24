960c2369b774148b934fe71a652d9ab3ce48e884
Fabio Ferreirinha <fabio.fereirinha@gmail.com>
2022-06-24 16:10:05 +0100

    Make CSS Changes
    Switched from flex to cssgrid for keypad

--- calculator.js
+++ calculator.js
@@ -9,6 +9,14 @@ const six = document.getElementById('six');
 const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
 const nine = document.getElementById('nine');
+const numbers = document.getElementsByClassName('numbers');
+
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const times = document.getElementById('times');
+const divisor = document.getElementById('divide');
+const equal = document.getElementById('equal');
+
 
 function add(a, b) {
     return a + b;
@@ -44,69 +52,79 @@ function operate(a, b, operator) {
     }
 }
 
-function buttonZero() {
-    let currentNum = 0;
-    populateDisplay('0');
-    return currentNum;
-}
-function buttonOne() {
-    let currentNum = 1;
-    populateDisplay('1');
-    return currentNum;
-}
-function buttonTwo() {
-    let currentNum = 2;
-    populateDisplay('2');
-    return currentNum;
-}
-function buttonThree() {
-    let currentNum = 3;
-    populateDisplay('3');
-    return currentNum;
-}
-function buttonFour() {
-    let currentNum = 4;
-    populateDisplay('4');
-    return currentNum;
-}
-function buttonFive() {
-    let currentNum = 5;
-    populateDisplay('5');
-    return currentNum;
-}
-function buttonSix() {
-    let currentNum = 6;
-    populateDisplay('6');
-    return currentNum;
-}
-function buttonSeven() {
-    let currentNum = 7;
-    populateDisplay('7');
-    return currentNum;
-}
-function buttonEight() {
-    let currentNum = 8;
-    populateDisplay('8');
-    return currentNum;
-}
-function buttonNine() {
-    let currentNum = 9;
-    populateDisplay('9');
-    return currentNum;
-}
-
 function populateDisplay(a) {
     console.log(a);
     screen.innerHTML = a;
 }
 
-zero.addEventListener("click", buttonZero);
-one.addEventListener("click", buttonOne);
-two.addEventListener("click", buttonTwo);
-three.addEventListener("click", buttonThree);
-four.addEventListener("click", buttonFour);
-five.addEventListener("click", buttonFive);
-six.addEventListener("click", buttonSix);
-seven.addEventListener("click", buttonSeven);
-eight.addEventListener("click", buttonEight);
-nine.addEventListener("click", buttonNine);
+zero.addEventListener("click", () => {
+    let currentNum = 0;
+    populateDisplay('0');
+    return currentNum;
+});
+one.addEventListener("click", () => {
+    let currentNum = 1;
+    populateDisplay('1');
+    return currentNum;
+});
+two.addEventListener("click", () => {
+    let currentNum = 2;
+    populateDisplay('2');
+    return currentNum;
+});
+three.addEventListener("click", () => {
+    let currentNum = 3;
+    populateDisplay('3');
+    return currentNum;
+});
+four.addEventListener("click", () => {
+    let currentNum = 4;
+    populateDisplay('4');
+    return currentNum;
+});
+five.addEventListener("click", () => {
+    let currentNum = 5;
+    populateDisplay('5');
+    return currentNum;
+});
+six.addEventListener("click", () => {
+    let currentNum = 6;
+    populateDisplay('6');
+    return currentNum;
+});
+seven.addEventListener("click", () => {
+    let currentNum = 7;
+    populateDisplay('7');
+    return currentNum;
+});
+eight.addEventListener("click", () => {
+    let currentNum = 8;
+    populateDisplay('8');
+    return currentNum;
+});
+nine.addEventListener("click", () => {
+    let currentNum = 9;
+    populateDisplay('9');
+    return currentNum;
+});
+
+plus.addEventListener("click", () => {
+    let currentOperator = '+';
+    return currentOperator;
+})
+minus.addEventListener("click", () => {
+    let currentOperator = '-';
+    return currentOperator;
+})
+times.addEventListener("click", () => {
+    let currentOperator = 'x';
+    return currentOperator;
+})
+divisor.addEventListener("click", () => {
+    let currentOperator = '/';
+    return currentOperator;
+})
+
+equal.addEventListener("click", () => {
+
+})
